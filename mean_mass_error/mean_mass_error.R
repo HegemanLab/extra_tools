ae6e35efb39daa0e9605d08cb6eb3cd862db8bf5
eslerm <eslerm@umn.edu>
2018-05-12 07:39:08 -0500

    added tabular output support. cleaned input format

--- mean_mass_error/mean_mass_error.R
+++ mean_mass_error/mean_mass_error.R
@@ -11,7 +11,8 @@
 # Each row corresponds to a standard.
 # See example input and output files.
 
-df <- read.table('input-data.tsv', header=T)
+df <- read.table('input-data.tsv',header=T)
+#df <- read.table('output-data.tsv', header=T)
 
 # Calculate PPM mass error for standards in each sample.
 # Append mass errors as a column to df per sample.
@@ -19,7 +20,7 @@ i = 1
 ppm <- data.frame()
 for(mass in df[,-c(1:3)]) {
   ppm <- c(abs((df$known_mass-mass)/df$known_mass*1e6))
-  name = paste(colnames(df[3+i]), "_ppm", sep="")
+  name = paste(colnames(df[3+i]),"_ppm",sep="")
   df[[name]] <- ppm
   i=i+1
 }
@@ -31,4 +32,4 @@ df$avg_ppm <- c(rowMeans(df[,(3+n):(3+2*n)]))
 
 print(paste("Average PPM mass error:",mean(df$avg_ppm)))
 
-write.csv(df, file='output-data.csv')
+write.table(df,file='output-data.tsv',sep="\t",row.names=F)
