375943577214fbfa07fb3f2a3adf77f839bf51bf
eslerm <eslerm@umn.edu>
2018-05-12 06:19:28 -0500

    Cleaning up documentation

--- mean_mass_error/mean_mass_error.R
+++ mean_mass_error/mean_mass_error.R
@@ -1,31 +1,31 @@
-# Mark Esler, Hegeman Lab. UMN
+# Hegeman Lab UMN
+# Mark Esler, 2018
 #
 # Calculate mean mass error of a mass spectrometry run
-# by comparing  observed and exact masses
+# by comparing observed and known masses of standards.
 #
-# The first three columns of tabular data must be: (1) 
-# standard (2) formula and (3) exact_mass
-#
-# All succeeding columns are samples of observed masses 
-# with each row corresponding to a standard
-#
-# See example files
+# The first three columns of the input file are named:
+# (1) standard (2) formula & (3) known_mass.
+# All succeeding columns are samples.
+# Sample columns can be given any unique name.
+# Each row corresponds to a standard.
+# See example input and output files.
 
 df <- read.table('input-data.tsv', header=T)
 
-# Calculate PPM mass error for standards in each sample
-# append mass errors as column to df per sample
+# Calculate PPM mass error for standards in each sample.
+# Append mass errors as a column to df per sample.
 i = 1
 ppm <- data.frame()
-for(mass in df[,-c(0:3)]) {
+for(mass in df[,-c(1:3)]) {
   name = paste(colnames(df[3+i]), "_ppm", sep="")
-  ppm <- c(abs((df$known_mass - mass)/df$exact_mass*1e6))
+  ppm <- c(abs((df$known_mass-mass)/df$known_mass*1e6))
   df[[name]] <- ppm
   i=i+1
 }
 
-# Calculate mean mass error for each standard append as
-# column to df
+# Calculate mean mass error for each standard.
+# Append as column to df.
 n = (length(df)-3)/2
 df$avg_ppm <- c(rowMeans(df[,(3+n):(3+2*n)]))
 
